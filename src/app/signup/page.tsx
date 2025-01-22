10f9fe9a633b8a423f4d48ba2d771be71f2481a7
sungwonnoh <jessica20730@naver.com>
2025-01-22 14:02:03 +0900

    feat: 학번이미지 예시 모달창

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -7,6 +7,7 @@ import { MdCameraEnhance } from "react-icons/md";
 import { useRef, useState } from "react";
 import { useRouter } from "next/navigation";
 import NextBtn from "@/components/button/nextBtn";
+import ExampleImg from "@/components/modal/exampleImg";
 
 interface PictureSpaceProps {
   isActive: boolean;
@@ -83,6 +84,7 @@ export default function Signup() {
   const [isActive, setIsActive] = useState(false);
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
   const fileInputRef = useRef<HTMLInputElement | null>(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const router = useRouter();
 
   const handlePictureClick = () => {
@@ -95,6 +97,9 @@ export default function Signup() {
       setIsActive(true);
     }
   };
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
   return (
     <Wrapper>
       <Header>
@@ -106,7 +111,7 @@ export default function Signup() {
         <SubTitle>
           한국외국어대학교 모바일 ID 화면을 캡쳐 후 업로드 해주세요
         </SubTitle>
-        <SubButton>
+        <SubButton onClick={toggleModal}>
           <BsFillQuestionCircleFill />
         </SubButton>
       </SubTitleContainer>
@@ -130,6 +135,7 @@ export default function Signup() {
         onChange={handleFileChange}
       />
       <NextBtn />
+      {isModalOpen && <ExampleImg onClose={toggleModal}></ExampleImg>}
     </Wrapper>
   );
 }
---
+++ src/components/modal/exampleImg.tsx
@@ -0,0 +1,59 @@
+"use client";
+import ReactDOM from "react-dom";
+import { theme } from "@/app/styles/theme";
+import { IoMdCloseCircle } from "react-icons/io";
+import styled from "styled-components";
+
+const ModalOverlay = styled.div`
+  position: fixed;
+  background-color: rgba(58, 58, 58, 0.6);
+  top: 0;
+  left: 0;
+  width: 100%;
+  //max-width: 31rem;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 999;
+`;
+const ModalContent = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 21.875rem;
+  height: 35.4375rem;
+  flex-shrink: 0;
+  border-radius: 0.625rem;
+  background: ${theme.colors.contrast};
+  box-shadow: 0px 0px 10px 0px rgba(142, 142, 142, 0.25);
+`;
+const Button = styled.button`
+  position: absolute;
+  top: 1rem;
+  right: 1rem;
+  color: ${theme.colors.primary};
+  font-size: 2rem;
+`;
+const Img = styled.img`
+  width: 14rem;
+  height: 28.6875rem;
+  object-fit: contain;
+`;
+interface ModalProps {
+  onClose: () => void;
+}
+export default function ExampleImg({ onClose }: ModalProps) {
+  return ReactDOM.createPortal(
+    <ModalOverlay>
+      <ModalContent>
+        <Button onClick={onClose}>
+          <IoMdCloseCircle />
+        </Button>
+        <Img src="/example.svg" />
+      </ModalContent>
+    </ModalOverlay>,
+    document.body
+  );
+}
