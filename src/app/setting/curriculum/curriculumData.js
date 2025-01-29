b6409ad3e9003fe2465e8d556cdd5d4e25f64ef4
sungwonnoh <jessica20730@naver.com>
2025-01-29 22:49:14 +0900

    docs: 커리큘럼 json추가

--- src/app/setting/curriculum/curriculumData.js
+++ src/app/setting/curriculum/curriculumData.js
@@ -5,7 +5,118 @@ export const curriculumData = [
       {
         id: "second",
         name: "전공교양",
-        subjects: [{ name: `컴퓨팅사고\n(파이썬)` }],
+        subjects: [
+          { name: `컴퓨팅사고\n(파이썬)` },
+          { name: `컴퓨터\n 프로그래밍 (C)` },
+        ],
+      },
+      {
+        id: "third",
+        name: "전공선택 일반",
+        subjects: [{ name: "이산수학" }],
+      },
+    ],
+  },
+  {
+    semester: "1-2",
+    tracks: [
+      {
+        id: "third",
+        name: "전공선택 일반",
+        subjects: [
+          {
+            name: "컴퓨터시스템입문",
+          },
+          {
+            name: "선형대수",
+          },
+        ],
+      },
+      {
+        id: "first",
+        name: "전공필수",
+        subjects: [{ name: `컴퓨터 프로그래밍\n 및 실습 (C++)` }],
+      },
+    ],
+  },
+  {
+    semester: "2-1",
+    tracks: [
+      {
+        id: "fifth",
+        name: "전공선택 전자",
+        subjects: [{ name: "전자기학" }, { name: "전기회로" }],
+      },
+      {
+        id: "third",
+        name: "전공선택 일반",
+        subjects: [{ name: "공업수학1" }, { name: "논리회로" }],
+      },
+      {
+        id: "first",
+        name: "전공필수",
+        subjects: [{ name: "자료구조" }],
+      },
+      {
+        id: "fourth",
+        name: "전공선택 컴퓨터",
+        subjects: [{ name: "프로그래밍어론" }, { name: `오픈소스SW및\n 실습` }],
+      },
+    ],
+  },
+  {
+    semester: "2-2",
+    tracks: [
+      {
+        id: "fifth",
+        name: "전공선택 전자",
+        subjects: [{ name: `전자공학 및 \n 실험` }],
+      },
+      {
+        id: "third",
+        name: "전공선택 일반",
+        subjects: [
+          { name: "공업수학2" },
+          { name: `마이크로프로세서\n 및실습` },
+          { name: "확률과통계" },
+        ],
+      },
+      {
+        id: "fourth",
+        name: "전공선택 컴퓨터",
+        subjects: [
+          { name: "알고리즘" },
+          { name: `객체지향\n 프로그래밍` },
+          { name: "웹프로그래밍" },
+        ],
+      },
+    ],
+  },
+  {
+    semester: "3-1",
+    tracks: [
+      {
+        id: "fifth",
+        name: "전공선택 전자",
+        subjects: [{ name: "제어공학" }, { name: "디지털신호처리" }],
+      },
+      {
+        id: "third",
+        name: "전공선택 일반",
+        subjects: [
+          { name: "컴퓨터구조" },
+          { name: `시스템\n프로그래밍` },
+          { name: "테이터통신" },
+        ],
+      },
+      {
+        id: "fourth",
+        name: "전공선택 컴퓨터",
+        subjects: [
+          { name: "데이터마이닝" },
+          { name: "컴퓨터그래픽스" },
+          { name: "설계패턴" },
+        ],
       },
     ],
   },
@@ -37,6 +148,68 @@ export const curriculumData = [
         id: "fifth",
         name: "전공선택 전자",
         subjects: [{ name: "전기기기" }],
+      },
+    ],
+  },
+  {
+    semester: "4-1",
+    tracks: [
+      {
+        id: "fifth",
+        name: "전공선택 전자",
+        subjects: [{ name: "IOT시스템" }],
+      },
+
+      {
+        id: "first",
+        name: "전공필수",
+        subjects: [{ name: `캡스톤설계및실습 \n(1,2학기)` }],
+      },
+      {
+        id: "fourth",
+        name: "전공선택 컴퓨터",
+        subjects: [{ name: "멀티고어컴퓨팅" }],
+      },
+      {
+        id: "third",
+        name: "전공선택 일반",
+        subjects: [
+          { name: "소셜네트워크분석" },
+          { name: `고급문제해결\n기법및실습` },
+          { name: "컴퓨터비전" },
+          { name: "빅데이터처리" },
+          { name: `데이터베이스\n설계` },
+          { name: "게임프로그래밍" },
+        ],
+      },
+    ],
+  },
+  {
+    semester: "4-2",
+    tracks: [
+      {
+        id: "fifth",
+        name: "전공선택 전자",
+        subjects: [{ name: "로봇공학" }],
+      },
+      {
+        id: "fourth",
+        name: "전공선택 컴퓨터",
+        subjects: [
+          { name: "컴퓨터보안" },
+          { name: `인간컴퓨터\n 상호작용` },
+          { name: "모바일프로그래밍" },
+          { name: "딥러닝" },
+          { name: `엔터프라이즈\n프로그래밍` },
+        ],
+      },
+      {
+        id: "third",
+        name: "전공선택 일반",
+        subjects: [
+          { name: `SW연구프로젝트\n 및실습` },
+          { name: "블록체인의이해" },
+        ],
       },
     ],
   },
