30ad024344be3fdc0fde2e7f7a4215bc471c4435
sungwonnoh <jessica20730@naver.com>
2025-03-05 23:57:40 +0900

    feat: 컴첵 인스타 링크 연결

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -87,7 +87,9 @@ export default function Login() {
     const baseURL = process.env.NEXT_PUBLIC_API_URL;
     window.location.href = `${baseURL}/oauth2/authorize/google`;
   };
-
+  const InstaLink= ()=>{
+    window.location.href="https://www.instagram.com/comncheck?igsh=dnRjOXdiaWpoN3Vo";
+  }
   return (
     <Wrapper>
       <Logo src={`/logo.png`} alt="로고" />
@@ -106,7 +108,7 @@ export default function Login() {
       </GoogleBtn>
       <StatusContainer>
         <AnsAccount>아직 학교 계정이 없으신가요?</AnsAccount>
-        <AskManager>관리자에게 문의하기</AskManager>
+        <AskManager onClick={InstaLink}>관리자에게 문의하기</AskManager>
       </StatusContainer>
     </Wrapper>
   );
