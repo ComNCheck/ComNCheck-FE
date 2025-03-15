f88aab9c74473a848c412ebf1780f3512193f3d0
yerimi <fhddl1019@naver.com>
2025-03-15 21:12:06 +0900

    fix : 공지 화면면 스크롤 영역 수정

--- src/app/notice/page.tsx
+++ src/app/notice/page.tsx
@@ -147,16 +147,16 @@ export default function Notice() {
       (eventDate.getTime() - today.getTime()) / (1000 * 60 * 60 * 24)
     );
 
-    if (diff===0) return "D-day";
-    if (diff<0) return "종료됨";
-    return `D-${diff}` ;
+    if (diff === 0) return "D-day";
+    if (diff < 0) return "종료됨";
+    return `D-${diff}`;
   };
 
   return (
     <ContainerWrapper>
-      <SlideHeader />
-
       <ContentContainer>
+        <SlideHeader />
+
         <HeaderContainer>
           <Header>
             <p onClick={handleEventClick}>
